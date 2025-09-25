9b436778c7ec8da000f3f0ffc95d917444d1f5da
FrancisWexler <support@wexlerllc.com>
2025-09-25 14:54:04 +0800

    hide cronjob

--- netlify/functions/send-scheduled-emails.mjs
+++ netlify/functions/send-scheduled-emails.mjs
@@ -1,13 +1,12 @@
-export const config = {
-  // run every 1 minute (UTC). Change to your cron as needed.
-  schedule: '*/1 * * * *'
-    // schedule: '0,30 19-21 * * 0' // 18-22 UTC on Sundays (1PM-5PM EST on Sundays)
-};
+// export const config = {
+//   // run every 1 minute (UTC). Change to your cron as needed.
+//   schedule: '*/1 * * * *'
+//     // schedule: '0,30 19-21 * * 0' // 18-22 UTC on Sundays (1PM-5PM EST on Sundays)
+// };
 
-export default async (req) => {
-  // scheduled functions receive a JSON payload with `next_run`
-  const { next_run } = await req.json();
-  console.log('Scheduled invocation — next_run:', next_run);
+// export default async (req) => {
+//   const { next_run } = await req.json();
+//   console.log('Scheduled invocation — next_run:', next_run);
 
-  return
-}
+//   return
+// }
